f74c29eb4a7b500a137b488f82b2f1d3cacc9a5b
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-12 21:08:13 -0400

    set up admin system

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,11 @@
 class PostsController < ApplicationController
   before_action :set_post, only: %i[ show edit update destroy ]
+  before_action :is_user?, only: %i[index show]
 
   # GET /posts or /posts.json
   def index
-    @posts = Post.all
+    #@posts = Post.all
+    @posts = Post.order(created_at: :desc).page(params[:page]).per(10)
   end
 
   # GET /posts/1 or /posts/1.json
@@ -21,29 +23,43 @@ class PostsController < ApplicationController
 
   # POST /posts or /posts.json
   def create
-    @post = Post.new(post_params)
-
-    respond_to do |format|
-      if @post.save
-        format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
-        format.json { render :show, status: :created, location: @post }
-      else
-        format.html { render :new, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+    if current_user.role == 'admin'
+      @post = Post.new(post_params)
+      @post.user_id = current_user.id
+      #if @post.images.attached?
+        @post.images.attach(params[:images])
+      #end
+      respond_to do |format|
+        if @post.save
+          format.html { redirect_to post_url(@post), notice: "Post was successfully created." }
+          format.json { render :show, status: :created, location: @post }
+        else
+          format.html { render :new, status: :unprocessable_entity }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      format.html { redirect_to @post, notice: "You do not have access to create a post."}
     end
   end
 
   # PATCH/PUT /posts/1 or /posts/1.json
   def update
-    respond_to do |format|
-      if @post.update(post_params)
-        format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
-        format.json { render :show, status: :ok, location: @post }
-      else
-        format.html { render :edit, status: :unprocessable_entity }
-        format.json { render json: @post.errors, status: :unprocessable_entity }
+    if current_user.role == 'admin'
+      #if @post.images.attached?
+        @post.images.attach(params[:images])
+      #end
+      respond_to do |format|
+        if @post.update(post_params)
+          format.html { redirect_to post_url(@post), notice: "Post was successfully updated." }
+          format.json { render :show, status: :ok, location: @post }
+        else
+          format.html { render :edit, status: :unprocessable_entity }
+          format.json { render json: @post.errors, status: :unprocessable_entity }
+        end
       end
+    else
+      format.html { redirect_to @post, notice: "You do not have access to create an edit."}
     end
   end
 
@@ -65,6 +81,9 @@ class PostsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def post_params
-      params.require(:post).permit(:title, :description, :content, :user_id)
+      params.require(:post).permit(:title, :description, :content, images: [])
+    end
+    def is_user?
+      user_signed_in? == false || current_user.role == 'user'
     end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
   resources :posts
   devise_for :users
+
+  get "about", to: "home#about"
+  get "home", to: "home#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "home#index"
 end
