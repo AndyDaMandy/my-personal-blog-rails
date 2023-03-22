c4f9bce0df06aab9a4c3febcecab4fa90736b026
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-22 19:49:41 -0400

    imported information from the old site

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,6 @@
 class Post < ApplicationRecord
-  validates :title, presence: true, length: {minimum: 5, maximum: 100 }
-  validates :description, presence: true, length:  {minimum: 4, maximum: 100}
+  validates :title, presence: true, length: {minimum: 1, maximum: 200 }
+  validates :description, presence: true, length:  {minimum: 1, maximum: 200}
   validates :content, presence: true, length: { minimum: 10 }
   belongs_to :user
   #has_many :comments, dependent: :destroy
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,6 +27,17 @@ Rails.application.configure do
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
 
+  #sendgrid
+  ActionMailer::Base.smtp_settings = {
+    :user_name => 'apikey',
+    :password => ENV["SENDGRID_API_KEY"], 
+    :domain => 'andy-m-blog.fly.dev',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
@@ -38,7 +49,7 @@ Rails.application.configure do
   # config.action_dispatch.x_sendfile_header = "X-Accel-Redirect" # for NGINX
 
   # Store uploaded files on the local file system (see config/storage.yml for options).
-  config.active_storage.service = :local
+  config.active_storage.service = :amazon
 
   # Mount Action Cable outside main process or domain.
   # config.action_cable.mount_path = nil
