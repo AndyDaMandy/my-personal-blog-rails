edbadf2ac044db7178ee8e5b8b2959d206209e99
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-23 19:25:02 -0400

    set up host

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -26,6 +26,7 @@ Rails.application.configure do
 
   # Compress CSS using a preprocessor.
   # config.assets.css_compressor = :sass
+  config.action_mailer.default_url_options = { host: 'andydamandy-blog.fly.dev' }
 
   #sendgrid
   ActionMailer::Base.smtp_settings = {
