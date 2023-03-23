b4ddbaa764c4a2c9034d2edb5a9e88caa9af78a0
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-23 16:31:51 -0400

    setting up user confirmation

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,3 +1,14 @@
 class CommentsController < ApplicationController
+    before_action authenticate_user!
+    def create
+        @post = Post.friendly.find(params[:id])
+        @comment = @post.comments.create(comment_params)
+        @comment.user_id = current_user.id
+        redirect_to post_path(@post)
+      end
     
+      private
+        def comment_params
+          params.require(:comment).permit(:comment, :user_id)
+        end
 end
