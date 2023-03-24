75cc8009d6dd016b240c92a723676000f72e083c
AndyDaMandy <maldonado.andy.m@gmail.com>
2023-03-23 21:58:03 -0400

    update devise email config

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,6 +3,6 @@ import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
 import "trix"
-import 'trix/dist/trix.css'
+//import 'trix/dist/trix.css'
 import "@rails/actiontext"
 //= require activestorage
