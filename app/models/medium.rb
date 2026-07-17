6a194d844e02c131670548d45ec7058531aefde0
AndyDaMandy <maldonado.andy.m@gmail.com>
2026-07-17 10:43:07 -0400

    commented out enums from medium

--- app/models/medium.rb
+++ app/models/medium.rb
@@ -3,13 +3,12 @@ class Medium < ApplicationRecord
   has_rich_text :thoughts
   belongs_to :user
 
-  enum category: %i[n/a other book movie tv game website video podcast music], _prefix: :media_type
-  enum platform: %i[n/a other physical streaming dvd blu-ray switch xbox playstation pc 3ds website youtube], _prefix: :media_platform
-  enum progress: %i[n/a other not_started in_progress finished], _prefix: :media_progress
+  # enum category: %i[n/a other book movie tv game website video podcast music], _prefix: :media_type
+  # enum platform: %i[n/a other physical streaming dvd blu-ray switch xbox playstation pc 3ds website youtube], _prefix: :media_platform
+  # enum progress: %i[n/a other not_started in_progress finished], _prefix: :media_progress
 
-  validates :name, presence: true
-  validates :release_date, presence: true
-  validates :category, presence: true
-  validates :platform, presence: true
-
+  # validates :name, presence: true
+  # validates :release_date, presence: true
+  # validates :category, presence: true
+  # validates :platform, presence: true
 end
